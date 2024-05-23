d6f2400fcc19db34bd28247e6d5c50011d6176f8
YuryK96 <kast2406@gmail.com>
2024-05-24 00:32:20 +0300

    fix error with adding user in DB when he is there already

--- db/user.js
+++ db/user.js
@@ -2,34 +2,39 @@ import {Knex} from "./connection.js";
 
 
 async function createTable() {
-	const tableExists = await Knex.schema.hasTable('users');
+    const tableExists = await Knex.schema.hasTable('users');
 
-	if (tableExists) {
-		return;
-	}
+    if (tableExists) {
+        return;
+    }
 
-	await Knex.schema.createTable('users', table => {
-		table.increments('id').primary();
-		table.text('userId').unique();
-		table.text('username');
-		table.text('access_token');
-		table.text('refresh_token');
-	});
+    await Knex.schema.createTable('users', table => {
+        table.increments('id').primary();
+        table.text('userId').unique();
+        table.text('username');
+        table.text('access_token');
+        table.text('refresh_token');
+    });
 }
 
 async function getById(id) {
-	const user = await Knex.from('users').select().where('userId', id);
+    const user = await Knex.from('users').select().where('userId', id);
 
-	return user;
+    return user;
 }
 
-async function add(username,userId, access_token, refresh_token) {
-	await Knex('users').insert({
-		username,
-		userId,
-		access_token,
-		refresh_token
-	});
+async function add(username, userId, access_token, refresh_token) {
+    const isExistUser = await getById(userId)
+    if (isExistUser.length) {
+        return
+    }
+
+    await Knex('users').insert({
+        username,
+        userId,
+        access_token,
+        refresh_token
+    });
 }
 
-export default {add,getById,createTable}
+export default {add, getById, createTable}
